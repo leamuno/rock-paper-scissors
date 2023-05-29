d35ef5a535c43a721f71a75e9e608f548e860bb6
Leandro <leandro.munozjr@gmail.com>
2023-05-29 17:49:59 +0900

    Create function that makes a choice for the computer player

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,11 @@
-console.log('test');
+function getComputerChoice() {
+    let computerRNG = Math.ceil(Math.random() * 3);
+
+    if (computerRNG == 1) {
+        return "Paper";
+    }else if (computerRNG == 2) {
+        return "Rock";
+    }else {
+        return "Scissors";
+    }
+}
