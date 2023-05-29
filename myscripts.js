744be69dd0294646ca3a888d4a2c288be120b93c
Leandro <leandro.munozjr@gmail.com>
2023-05-29 18:26:11 +0900

    Add a function that takes the player and computer selection to play a game of rock paper scissors

--- myscripts.js
+++ myscripts.js
@@ -2,10 +2,44 @@ function getComputerChoice() {
     let computerRNG = Math.ceil(Math.random() * 3);
 
     if (computerRNG == 1) {
-        return "Paper";
+        return "paper";
     }else if (computerRNG == 2) {
-        return "Rock";
+        return "rock";
     }else {
-        return "Scissors";
+        return "scissors";
     }
+}
+
+let computerSelection = getComputerChoice();
+
+let playerSelection = 'rock';
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return `It's a draw.`;
+    }else if (playerSelection == 'scissors') {
+        if (computerSelection == 'paper') {
+            return 'You Win! Scissors beats Paper';
+        }else {
+            return 'You Lose! Rock beats Scissors'
+        }
+    }else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            return 'You Lose! Paper beats Rock';
+        }else {
+            return 'You Win! Rock beats Scissors';
+        }
+    }else if (playerSelection == 'paper') {
+        if (computerSelection == 'scissors') {
+            return 'You Lose! Scissors beats paper'
+        }else {
+            return 'You win! Paper beats rock'
+        }
+    }
+}
+
+function game() {
+    computerSelection = getComputerChoice();
+    playerSelection = prompt('Choose rock, paper, or scissors:').toLowerCase();
+    console.log(playRound(playerSelection, computerSelection));
 }
